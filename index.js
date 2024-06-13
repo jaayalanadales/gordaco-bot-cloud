b4003c4cc182e43bd6673349bbb3bffe5b3d564e
jaayalanadales <jaayalanadales@gmail.com>
2024-06-13 19:31:07 +0200

    Cambiar puerto

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 import express from 'express'
 const app = express()
-const port = 8080;
+const port = 49279;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
